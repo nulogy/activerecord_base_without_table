99561510a4da5afae7bd183b26eebe09b311e5af
Ryan De Villa <ryand@nulogy.com>
2018-06-15 17:59:43 -0400

    Reorganize specs

--- spec/base_without_table_migration_harness_spec.rb
+++ spec/base_without_table_migration_harness_spec.rb
@@ -2,54 +2,56 @@ require 'spec_helper'
 require 'rails_helper'
 
 RSpec.describe ActiveRecord::BaseWithoutTable do
-  it 'allows specification of its own attributes' do
-    base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
-      column :created_at, :datetime
-      column :due_on, :date
-      column :start_time, :time
-      column :external_id, :integer
-      column :is_something, :boolean
-      column :number, :decimal
-      column :description, :text
-    end
-
-    instance = base_without_table_class.new(
-      created_at: '2019-01-01 00:00:00.000000',
-      due_on: '2020-01-01',
-      start_time: '00:00:00',
-      external_id: '1',
-      is_something: 't',
-      number: '1.2345678901',
-      description: 'My description'
-    )
-
-    expect(instance).to have_attributes(
-      created_at: Time.zone.parse('2019-01-01T00:00:00Z'),
-      due_on: Date.parse('2020-01-01'),
-      start_time: Time.zone.parse('2000-01-01T00:00:00Z'),
-      external_id: 1,
-      is_something: true,
-      number: BigDecimal('1.2345678901'),
-      description: 'My description'
-    )
-  end
-
-  it 'allows specification of default values for attributes' do
-    base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
-      column :number, :integer, 1
-    end
-
-    instance = base_without_table_class.new
-
-    expect(instance.number).to eq(1)
-  end
-
-  it 'supports the legacy datetime point column type, whose value will never actually be accessed' do
-    expect do
+  context 'when specifying columns' do
+    it 'allows specification of its own attributes' do
       base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
-        column :snapshot_at, :datetime_point
+        column :created_at, :datetime
+        column :due_on, :date
+        column :start_time, :time
+        column :external_id, :integer
+        column :is_something, :boolean
+        column :number, :decimal
+        column :description, :text
       end
-    end.to_not raise_error
+
+      instance = base_without_table_class.new(
+        created_at: '2019-01-01 00:00:00.000000',
+        due_on: '2020-01-01',
+        start_time: '00:00:00',
+        external_id: '1',
+        is_something: 't',
+        number: '1.2345678901',
+        description: 'My description'
+      )
+
+      expect(instance).to have_attributes(
+        created_at: Time.zone.parse('2019-01-01T00:00:00Z'),
+        due_on: Date.parse('2020-01-01'),
+        start_time: Time.zone.parse('2000-01-01T00:00:00Z'),
+        external_id: 1,
+        is_something: true,
+        number: BigDecimal('1.2345678901'),
+        description: 'My description'
+      )
+    end
+
+    it 'allows specification of default values for attributes' do
+      base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
+        column :number, :integer, 1
+      end
+
+      instance = base_without_table_class.new
+
+      expect(instance.number).to eq(1)
+    end
+
+    it 'supports the legacy datetime point column type, whose value will never actually be accessed' do
+      expect do
+        base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
+          column :snapshot_at, :datetime_point
+        end
+      end.to_not raise_error
+    end
   end
 
   context 'when enforcing validations' do
@@ -206,93 +208,65 @@ RSpec.describe ActiveRecord::BaseWithoutTable do
       end
     end
   end
-  
-  it 'can be associated to other ActiveRecords' do
-    base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
-      column :model_id, :integer
-      belongs_to :model
 
-      def self.name
-        'BaseWithoutTableInstance'
+  context 'when specifying associations' do
+    it 'can be associated to other ActiveRecords' do
+      base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
+        column :model_id, :integer
+        belongs_to :model
+
+        def self.name
+          'BaseWithoutTableInstance'
+        end
       end
-    end
-    model = Model.create!(description: 'Something')
+      model = Model.create!(description: 'Something')
 
-    base_without_table = base_without_table_class.new(
-      model_id: model.id
-    )
+      base_without_table = base_without_table_class.new(
+        model_id: model.id
+      )
 
-    expect(base_without_table.model).to eq(model)
-  end
-
-  it 'can be associated to other ActiveRecords by explicitly specifying the associated class name' do
-    base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
-      column :first_model_id, :integer
-      column :second_model_id, :integer
-      belongs_to :first_model, class_name: 'Model'
-      belongs_to :second_model, class_name: 'Model'
-
-      def self.name
-        'BaseWithoutTableInstance'
-      end
-    end
-    first_model = Model.create!(description: 'First model')
-    second_model = Model.create!(description: 'Second model')
-
-    base_without_table = base_without_table_class.new(
-      first_model_id: first_model.id,
-      second_model_id: second_model.id
-    )
-
-    expect(base_without_table.first_model).to eq(first_model)
-    expect(base_without_table.second_model).to eq(second_model)
-  end
-
-  it 'can be associated to other ActiveRecords by explicitly specifying the foreign key attribute' do
-    base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
-      column :my_model_id, :integer
-      belongs_to :model, foreign_key: 'my_model_id'
-
-      def self.name
-        'BaseWithoutTableInstance'
-      end
-    end
-    model = Model.create!(description: 'First model')
-
-    base_without_table = base_without_table_class.new(my_model_id: model.id)
-
-    expect(base_without_table.model).to eq(model)
-  end
-
-  it 'allows construction of records given a SQL statement' do
-    base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
-      column :model_description, :text
-
-      def self.name
-        'BaseWithoutTableInstance'
-      end
-    end
-    Model.create!(description: 'Something')
-
-    base_without_table = base_without_table_class.find_by_sql(<<-SQL)
-    SELECT description AS model_description FROM models
-    SQL
-
-    expect(base_without_table.first.model_description).to eq('Something')
-  end
-
-  it 'generates predicates for boolean columns' do
-    base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
-      column :is_something, :boolean
-
-      def self.name
-        'BaseWithoutTableInstance'
-      end
+      expect(base_without_table.model).to eq(model)
     end
 
-    base_without_table = base_without_table_class.new(is_something: true)
+    it 'can be associated to other ActiveRecords by explicitly specifying the associated class name' do
+      base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
+        column :first_model_id, :integer
+        column :second_model_id, :integer
+        belongs_to :first_model, class_name: 'Model'
+        belongs_to :second_model, class_name: 'Model'
 
-    expect(base_without_table.is_something?).to be(true)
+        def self.name
+          'BaseWithoutTableInstance'
+        end
+      end
+      first_model = Model.create!(description: 'First model')
+      second_model = Model.create!(description: 'Second model')
+
+      base_without_table = base_without_table_class.new(
+        first_model_id: first_model.id,
+        second_model_id: second_model.id
+      )
+
+      expect(base_without_table.first_model).to eq(first_model)
+      expect(base_without_table.second_model).to eq(second_model)
+    end
+
+    it 'can be associated to other ActiveRecords by explicitly specifying the foreign key attribute' do
+      base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
+        column :my_model_id, :integer
+        belongs_to :model, foreign_key: 'my_model_id'
+
+        def self.name
+          'BaseWithoutTableInstance'
+        end
+      end
+      model = Model.create!(description: 'First model')
+
+      base_without_table = base_without_table_class.new(my_model_id: model.id)
+
+      expect(base_without_table.model).to eq(model)
+    end
+
   end
 
   context 'when using ActiveRecord callbacks' do
@@ -339,6 +313,37 @@ RSpec.describe ActiveRecord::BaseWithoutTable do
       expect(base_without_table).to be_valid
       expect(base_without_table.number).to eq(0)
     end
+  end
+
+  it 'allows construction of records given a SQL statement' do
+    base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
+      column :model_description, :text
+
+      def self.name
+        'BaseWithoutTableInstance'
+      end
+    end
+    Model.create!(description: 'Something')
+
+    base_without_table = base_without_table_class.find_by_sql(<<-SQL)
+    SELECT description AS model_description FROM models
+    SQL
+
+    expect(base_without_table.first.model_description).to eq('Something')
+  end
+
+  it 'generates predicates for boolean columns' do
+    base_without_table_class = Class.new(ActiveRecord::BaseWithoutTable) do
+      column :is_something, :boolean
+
+      def self.name
+        'BaseWithoutTableInstance'
+      end
+    end
+
+    base_without_table = base_without_table_class.new(is_something: true)
+
+    expect(base_without_table.is_something?).to be(true)
   end
 
   context 'migrated legacy tests' do
