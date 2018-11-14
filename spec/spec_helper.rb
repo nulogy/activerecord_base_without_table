8abc7501d49d1b5c78ff867db24b028be8596c47
Ryan De Villa <ryand@nulogy.com>
2018-11-14 14:50:09 -0500

    Enable the RSpec focus filter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,6 +46,7 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.filter_run_when_matching :focus
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
@@ -55,7 +56,6 @@ RSpec.configure do |config|
   # is tagged with `:focus`, all examples get run. RSpec also provides
   # aliases for `it`, `describe`, and `context` that include `:focus`
   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
-  config.filter_run_when_matching :focus
 
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
