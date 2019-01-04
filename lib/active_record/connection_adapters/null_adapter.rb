69274e8f6341c3fa7b3ae2a9f089bc66af407bb7
Ryan De Villa <ryand@nulogy.com>
2019-01-04 14:21:29 -0500

    Reorganize comments

--- lib/active_record/connection_adapters/null_adapter.rb
+++ lib/active_record/connection_adapters/null_adapter.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
+# This class is a concrete implementation of ConnectionAdapters::AbstractAdapter
+# that eliminates all code paths attempting to open a connection to a real
+# database backend.
 module ActiveRecord
   module ConnectionAdapters
-    # This class is a concrete implementation of ConnectionAdapters::AbstractAdapter
-    # that eliminates all code paths attempting to open a connection to a real
-    # database backend.
     class NullAdapter < SimpleDelegator
       def schema_cache
         NullSchemaCache.new
--- lib/active_record/connection_adapters/null_schema_cache.rb
+++ lib/active_record/connection_adapters/null_schema_cache.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
+# When run against a real database backend, ordinarily the SchemaCache
+# would open a connection to the DB and attempt to retrieve schema
+# information from it.
+#
+# Here, we declare that these ActiveRecord classes do not have any
+# database-backed schema or column information. The structure of an
+# ActiveRecord::BaseWithoutTable is purely an in-memory construct
+# and is defined through the use of the `column` class methods.
 module ActiveRecord
   module ConnectionAdapters
-    # When run against a real database backend, ordinarily the SchemaCache
-    # would open a connection to the DB and attempt to retrieve schema
-    # information from it.
-    #
-    # Here, we declare that these ActiveRecord classes do not have any
-    # database-backed schema or column information. The structure of an
-    # ActiveRecord::BaseWithoutTable is purely an in-memory construct
-    # and is defined through the use of the `column` class methods.
     class NullSchemaCache
       def columns(*args)
         []
