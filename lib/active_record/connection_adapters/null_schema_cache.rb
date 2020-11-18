1ee696af07896593cff156febd02d79d3eb9053a
Alistair McKinnell <alistairm@nulogy.com>
2020-11-18 06:50:43 -0500

    Add the #columns_hash? method
    - A change in the Rails 6.0 ActiveRecord API

--- lib/active_record/connection_adapters/null_schema_cache.rb
+++ lib/active_record/connection_adapters/null_schema_cache.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+#
 # When run against a real database backend, ordinarily the SchemaCache
 # would open a connection to the DB and attempt to retrieve schema
 # information from it.
@@ -8,15 +9,23 @@
 # database-backed schema or column information. The structure of an
 # ActiveRecord::BaseWithoutTable is purely an in-memory construct
 # and is defined through the use of the `column` class methods.
+#
 module ActiveRecord
   module ConnectionAdapters
     class NullSchemaCache
+      # Callers operate with no columns.
       def columns(*)
         []
       end
 
+      # Callers operate with no columns.
       def columns_hash(*)
         {}
+      end
+
+      # Callers consider the columns cached.
+      def columns_hash?(*)
+        true
       end
     end
   end
