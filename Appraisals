7eb6d4706d90c38314e3ac9f9320137cc356e781
Alistair McKinnell <alistairm@nulogy.com>
2025-10-06 05:34:42 -0400

    Drop support for Rails 7.1

--- Appraisals
+++ Appraisals
@@ -1,7 +1,3 @@
-appraise "rails-7-1" do
-  gem "rails", "7.1.3.4"
-end
-
 appraise "rails-7-2" do
   gem "rails", "7.2.2.1"
 end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2018_05_25_183549) do
+ActiveRecord::Schema[7.1].define(version: 2018_05_25_183549) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,4 +19,5 @@ ActiveRecord::Schema[7.0].define(version: 2018_05_25_183549) do
     t.datetime "updated_at", null: false
     t.text "description"
   end
+
 end
